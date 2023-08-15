6173e39884c712e3dff5c39835cf9c089ecc88aa
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2023-08-15 22:38:50 +0600

    first commit

---
+++ module 2/javascript/app.js
@@ -0,0 +1,12 @@
+document.getElementById('red').addEventListener('click', function(){
+    document.body.style.backgroundColor='red';
+})
+document.getElementById('green').addEventListener('click', function(){
+    document.body.style.backgroundColor='green';
+})
+document.getElementById('yellow').addEventListener('click', function(){
+    document.body.style.backgroundColor='yellow';
+})
+document.getElementById('orange').addEventListener('click', function(){
+    document.body.style.backgroundColor='orange';
+})
